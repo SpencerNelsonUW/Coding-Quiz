d1f29b499dd1525d3733d836c9fcdbe78393c5ab
Spencer Nelson <spencernelsonn@gmail.com>
2022-08-28 13:47:19 -0700

    restructured HTML, added CSS and now starting on JS

---
+++ Assets/script.js
@@ -0,0 +1,16 @@
+const startButton = document.getElementById("startBtn")
+const questionContainer = document.getElementById("questionContainer")
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+    startButton.classList.add("hide")
+}
+
+function setNextQuestions() {
+
+}
+
+function selectAnswer() {
+
+}
